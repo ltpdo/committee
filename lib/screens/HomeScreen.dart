8aeece0bffcb83ff20893092a5625f5816a3fbcd
Yoshikawa Fuma <k21rs139@st.kyusan-u.ac.jp>
2023-09-02 16:48:56 +0900

    アカウント作成画面/ログイン画面から戻れないように修正

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -41,74 +41,78 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('コミティ'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.red,
-            height: 265,
-            child: Column(
-              children: [
-                SizedBox(height: 12),
-                Title(
-                  color: Colors.black,
-                  child: Text(
-                    '所属しているコミュニティ',
-                  ),
-                ),
-                SizedBox(height: 20),
-                belongToCommunityList(communities: dummy_community),
-                SizedBox(height: 6),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('もっと見る'),
-                ),
-              ],
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('コミティ'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
             ),
-          ),
-          SizedBox(height: 50),
-          Container(
-            color: Colors.blue,
-            height: 265,
-            child: Column(
-              children: [
-                SizedBox(height: 12),
-                Title(
-                  color: Colors.black,
-                  child: Text(
-                    'おすすめのコミュニティ',
+          ],
+        ),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.red,
+              height: 265,
+              child: Column(
+                children: [
+                  SizedBox(height: 12),
+                  Title(
+                    color: Colors.black,
+                    child: Text(
+                      '所属しているコミュニティ',
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                belongToCommunityList(communities: dummy_community),
-                SizedBox(height: 6),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('もっと見る'),
-                ),
-              ],
+                  SizedBox(height: 20),
+                  belongToCommunityList(communities: dummy_community),
+                  SizedBox(height: 6),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('もっと見る'),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム画面'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: '検索'),
-          BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'メッセージ'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'アカウント'),
-        ],
-        type: BottomNavigationBarType.fixed,
+            SizedBox(height: 50),
+            Container(
+              color: Colors.blue,
+              height: 265,
+              child: Column(
+                children: [
+                  SizedBox(height: 12),
+                  Title(
+                    color: Colors.black,
+                    child: Text(
+                      'おすすめのコミュニティ',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  belongToCommunityList(communities: dummy_community),
+                  SizedBox(height: 6),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('もっと見る'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _onItemTapped,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム画面'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: '検索'),
+            BottomNavigationBarItem(icon: Icon(Icons.mail), label: 'メッセージ'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'アカウント'),
+          ],
+          type: BottomNavigationBarType.fixed,
+        ),
       ),
     );
   }
