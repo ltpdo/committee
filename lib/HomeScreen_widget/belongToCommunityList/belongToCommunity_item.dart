f946ae88f559690ff3b3aa3e9d55edd172776f72
ltpdo <fgh2u58h42xctzxr32x@gmail.com>
2023-09-04 23:13:17 +0900

    動作確認ずみ

--- lib/HomeScreen_widget/belongToCommunityList/belongToCommunity_item.dart
+++ lib/HomeScreen_widget/belongToCommunityList/belongToCommunity_item.dart
@@ -15,12 +15,24 @@ class belongToCommunityItem extends StatelessWidget {
           horizontal: 20,
           vertical: 16,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
+            IconTheme(
+              data: IconThemeData(size: 30),
+              child: dummy_community[0].circlepicture,
+            ),
+            const SizedBox(width: 10),
             Text(
               dummy_community[0].circlename,
               style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(width: 100),
+            Column(
+              children: [
+                Text('日時：' + dummy_community[0].circleduration),
+                Text('場所：' + dummy_community[0].circleplace),
+              ],
             ),
             const SizedBox(height: 4),
           ],
