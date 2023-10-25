d193530b3001def9c0abebcde3acc7b463fc0468
shimmp <141289074+shimmp@users.noreply.github.com>
2023-10-25 20:52:42 +0900

    変更内容を記録

--- lib/view/account_screen.dart
+++ lib/view/account_screen.dart
@@ -1,8 +1,7 @@
+import 'package:committee/component/account_screem_widget/sign_out.dart';
 import 'package:flutter/material.dart';
 import 'package:committee/component/account_screem_widget/paswordchaging_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:committee/component/account_screem_widget/sign_out.dart';
-import 'package:committee/component/account_screem_widget/account_name.dart';
 
 class AccountScreen extends StatelessWidget {
   const AccountScreen({Key? key}) : super(key: key);
@@ -13,127 +12,136 @@ class AccountScreen extends StatelessWidget {
     final uid = auth.currentUser?.uid ?? "DefaultUID";
     final mad = auth.currentUser?.email ?? "DefaultEmail";
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('コミティ'),
-      ),
-      body: Form(
-        child: Column(
-          children: <Widget>[
-            //始まり
-            Container(
-              width: 130,
-              margin: const EdgeInsets.fromLTRB(0, 0, 270, 0),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(64, 57, 58, 59),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              //child: account_name()
-            ),
-
-            Container(
-              width: 500,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.black,
-                    width: 3,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('コミティ'),
+        ),
+        body: Form(
+          child: Column(
+            children: <Widget>[
+              //始まり
+              Container(
+                width: 130,
+                margin: const EdgeInsets.fromLTRB(0, 0, 270, 0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(64, 57, 58, 59),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Text(
+                  'ユーザー名',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.normal,
                   ),
                 ),
               ),
-              child: Text(
-                mad,
-                style: const TextStyle(fontSize: 20),
-              ),
-            ),
-            Container(
-              height: 20, //空白
-              color: const Color.fromARGB(255, 255, 255, 255),
-              alignment: Alignment.center,
-              child: const Text(''),
-            ),
-            Container(
-              width: 130,
-              margin: const EdgeInsets.fromLTRB(0, 0, 270, 0),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(64, 57, 58, 59),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Text(
-                'ユーザーid',
-                style: TextStyle(fontSize: 25),
-              ),
-            ),
-            Container(
-              width: 500,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.black,
-                    width: 3,
-                  ),
-                ),
-              ),
-              child: Text(
-                uid,
-                style: const TextStyle(fontSize: 20),
-              ),
-            ),
 
-            Container(
-              height: 20, //空白
-              color: const Color.fromARGB(255, 255, 255, 255),
-              alignment: Alignment.center,
-              child: const Text(''),
-            ),
-
-            Container(
-              //パスワード変更
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(64, 57, 58, 59),
-                borderRadius: BorderRadius.circular(50),
-              ),
-
-              width: 250,
-              height: 70,
-              alignment: Alignment.center,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SettingScreen(),
+              Container(
+                width: 500,
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.black,
+                      width: 3,
                     ),
-                  );
-                }, //ボタンを押した後の処理
-
-                style: TextButton.styleFrom(
-                  foregroundColor: const Color.fromRGBO(233, 50, 5, 0.705),
-                  textStyle: const TextStyle(
-                    fontSize: 30,
                   ),
                 ),
-                child: const Text('パスワード変更'),
+                child: Text(
+                  mad,
+                  style: const TextStyle(fontSize: 20),
+                ),
               ),
-            ),
-            Container(
-              height: 20, //空白
-              color: const Color.fromARGB(255, 255, 255, 255),
-              alignment: Alignment.center,
-              child: const Text(''),
-            ),
-            Container(
-              //サインアウトの処理
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(64, 57, 58, 59),
-                borderRadius: BorderRadius.circular(50),
+              Container(
+                height: 20, //空白
+                color: const Color.fromARGB(255, 255, 255, 255),
+                alignment: Alignment.center,
+                child: const Text(''),
               ),
-              width: 250,
-              height: 70,
-              alignment: Alignment.center,
-              child: const Sign_out(),
-            ),
-          ],
+              Container(
+                width: 130,
+                margin: const EdgeInsets.fromLTRB(0, 0, 270, 0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(64, 57, 58, 59),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  'ユーザーid',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              Container(
+                width: 500,
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.black,
+                      width: 3,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  uid,
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+
+              Container(
+                height: 20, //空白
+                color: const Color.fromARGB(255, 255, 255, 255),
+                alignment: Alignment.center,
+                child: const Text(''),
+              ),
+
+              Container(
+                //パスワード変更
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(64, 57, 58, 59),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+
+                width: 250,
+                height: 70,
+                alignment: Alignment.center,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SettingScreen(),
+                      ),
+                    );
+                  }, //ボタンを押した後の処理
+
+                  style: TextButton.styleFrom(
+                    foregroundColor: const Color.fromRGBO(233, 50, 5, 0.705),
+                    textStyle: const TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  child: const Text('パスワード変更'),
+                ),
+              ),
+              Container(
+                height: 20, //空白
+                color: const Color.fromARGB(255, 255, 255, 255),
+                alignment: Alignment.center,
+                child: const Text(''),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(64, 57, 58, 59),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                width: 250,
+                height: 70,
+                alignment: Alignment.center,
+                child: const Sign_out(),
+              ),
+            ],
+          ),
         ),
       ),
     );
