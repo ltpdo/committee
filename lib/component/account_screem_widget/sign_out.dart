4897ae26e15f7ed7c444956e334ebd5f6e248ebe
shimmp <141289074+shimmp@users.noreply.github.com>
2023-11-05 10:21:49 +0900

    サインアウト時にダイアログで確認画面がでる

--- lib/component/account_screem_widget/sign_out.dart
+++ lib/component/account_screem_widget/sign_out.dart
@@ -9,10 +9,33 @@ class SignOut extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextButton(
       onPressed: () {
-        FirebaseAuth.instance.signOut();
-        Navigator.pop(
-          context,
-        );
+        showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text("サインアウトしますか？"),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Cancel"),
+                  ),
+                  TextButton(
+                    child: const Text("OK"),
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      () => Navigator.pop(context);
+                      () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Welcome()));
+                    },
+                  ),
+                ],
+              );
+            });
       },
       style: TextButton.styleFrom(
         foregroundColor: const Color.fromRGBO(233, 50, 5, 0.705),
