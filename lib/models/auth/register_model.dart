fae91e879ef67168ea997c0a1a7df2c239fa0f5b
Yoshikawa Fuma <k21rs139@st.kyusan-u.ac.jp>
2023-11-22 19:20:16 +0900

    不要なimport文を削除

--- lib/models/auth/register_model.dart
+++ lib/models/auth/register_model.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:committee/view/register_name.dart';
-import 'package:committee/view/select_tag_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
