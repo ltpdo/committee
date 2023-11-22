65c4f9363c99a421a0e4529aac3283daa48fb904
Yoshikawa Fuma <k21rs139@st.kyusan-u.ac.jp>
2023-11-22 19:12:28 +0900

    ユーザーの写真を登録する機能を実装

--- lib/view/register_name.dart
+++ lib/view/register_name.dart
@@ -1,5 +1,7 @@
 import 'package:committee/models/auth/register_model.dart';
+import 'package:committee/view/select_tag_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:profile_image_selector/profile_image_selector.dart';
 
 class RegisterName extends StatefulWidget {
   final String urole;
@@ -13,68 +15,108 @@ class RegisterName extends StatefulWidget {
 
 class RegisterNameState extends State<RegisterName> {
   final RegisterModel register = RegisterModel();
+  final ProfileImageSelector userImage = ProfileImageSelector(
+    size: 100,
+    backgroundColor: Colors.grey,
+  );
+  bool _isLoading = false;
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return WillPopScope(
-      onWillPop: () async => false,
+      onWillPop: () async {
+        return false;
+      },
       child: GestureDetector(
           onTap: () {
             FocusScope.of(context).unfocus();
           },
           child: Scaffold(
-            body: Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "コミティ",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: size.width / 7,
-                      ),
-                    ),
-                    SizedBox(
-                      height: size.height / 12 * 2.8,
-                    ),
-                    //名前入力用テキストフィールド
-                    SizedBox(
-                      width: size.width / 7 * 6,
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(), labelText: "名前"),
-                        onChanged: (String value) {
-                          setState(() {
-                            register.setName(value);
-                          });
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: size.height / 5,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        register.registerName(context, widget.urole);
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red),
-                        elevation: MaterialStateProperty.all<double>(2.0),
-                      ),
-                      child: Text(
-                        "名前を登録",
+            body: Stack(children: [
+              Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "コミティ",
                         style: TextStyle(
-                          fontSize: size.width / 14,
-                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: size.width / 7,
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: size.height / 10,
+                      ),
+                      userImage,
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Text("使用する画像を選んでください"),
+                      SizedBox(
+                        height: size.height / 20,
+                      ),
+                      //名前入力用テキストフィールド
+                      SizedBox(
+                        width: size.width / 7 * 6,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(), labelText: "名前"),
+                          onChanged: (String value) {
+                            setState(() {
+                              register.setName(value);
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: size.height / 8.1,
+                      ),
+                      ElevatedButton(
+                        onPressed: !_isLoading
+                            ? () async {
+                                setState(() {
+                                  _isLoading = true;
+                                });
+                                await register
+                                    .registerNameAndPicture(
+                                      context,
+                                      userImage,
+                                    )
+                                    .then((_) => setState(() {
+                                          _isLoading = false;
+                                        }))
+                                    .then((_) => Navigator.push(context,
+                                            MaterialPageRoute(
+                                                builder: (context) {
+                                          return SelectTagScreen(
+                                            urole: widget.urole,
+                                          );
+                                        })));
+                              }
+                            : null,
+                        style: ButtonStyle(
+                          backgroundColor: !_isLoading
+                              ? MaterialStateProperty.all<Color>(Colors.red)
+                              : MaterialStateProperty.all<Color>(Colors.grey),
+                          elevation: MaterialStateProperty.all<double>(2.0),
+                        ),
+                        child: Text(
+                          "名前を登録",
+                          style: TextStyle(
+                            fontSize: size.width / 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              Center(
+                child: _isLoading ? const CircularProgressIndicator() : null,
+              )
+            ]),
           )),
     );
   }
